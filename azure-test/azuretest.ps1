625744ef3b398262d3da0aa1dc81c79468e5b5f2
Sam <109683132+kernelsam@users.noreply.github.com>
2025-03-04 12:28:25 -0800

    test

--- azure-test/azuretest.ps1
+++ azure-test/azuretest.ps1
@@ -23,8 +23,6 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 ./aws/install
 
-${Env:SENZING_VERSION} = '3.12.5-25031'
-
 if ( ${Env:ARCHITECTURE} -eq 'x86' ) {
   ${Env:DEB_ARCHITECTURE} = 'amd64'
   ${Env:RPM_ARCHITECTURE} = 'x86_64'
@@ -55,23 +53,46 @@ echo "[INFO] architecture is: ${Env:ARCHITECTURE}"
 echo "[INFO] rpm platform path is: ${Env:RPM_PLATFORM_PATH}"
 echo "[INFO] deb platform path is: ${Env:DEB_PLATFORM_PATH}"
 
-${Env:RPM_PATH} = "https://senzing-production-yum.s3.amazonaws.com/${Env:RPM_PLATFORM_PATH}/${Env:RPM_ARCHITECTURE}"
-${Env:DEB_PATH} = "https://senzing-production-apt.s3.amazonaws.com/${Env:DEB_PLATFORM_PATH}/s/se/"
+${Env:RPM_PATH} = "s3://senzing-production-yum/${Env:RPM_PLATFORM_PATH}/${Env:RPM_ARCHITECTURE}"
+${Env:DEB_PATH} = "s3://senzing-production-apt/${Env:DEB_PLATFORM_PATH}/s/se/"
+
+if ( ${Env:SENZING_VERSION} -eq 'latest' ) {
+  aws s3 ls "${Env:RPM_PATH}" --no-sign-request | grep "runtime" | awk '{print $NF}' >> packages
+  ${Env:SENZING_VERSION}$(cat packages | sort -r | head -n 1 | cut -d "-" -f 3)
+}
+else {
+  aws s3 ls "${Env:RPM_PATH}" --no-sign-request | awk '{print $NF}' | grep "${Env:SENZING_VERSION}"
+  exit_status=$?
+  if [ $exit_status -ne 0 ]; then
+    echo "[ERROR] Failed to find Senzing version: ${Env:SENZING_VERSION}."
+    echo "[ERROR] Please refer to https://senzing.com/releases/ for supported versions."
+    exit $exit_status
+  fi
+}
 
 $StorageAccount = Get-AzStorageAccount -ResourceGroupName ${Env:RESOURCEGROUP} -Name ${Env:STORAGEACCOUNT}
 
 mkdir rpm
 cd rpm
-aws s3 cp --recursive --exclude="*" --include="*.rpm" "${Env:RPM_PATH}" . --no-sign-request
-Get-ChildItem -File -Recurse | Set-AzStorageBlobContent -Container 'senzing' -Context $StorageAccount.Context
+packages=$(aws s3 ls ${Env:RPM_PATH} --no-sign-request | awk '{print $NF}' | grep "${Env:SENZING_VERSION}" | grep '.rpm')
+for package in ${packages}; do
+  echo "[INFO] download: $package"
+  aws s3 cp --exclude="*" --include="$package" "${Env:RPM_PATH}" . --no-sign-request
+  Set-AzStorageFileContent -ShareName 'senzing' -Source "$package" -Path "${Env:ARCHITECTURE}/openssl${Env:OPENSSLVERSION}" -Context $storageAccount.Context
+done
 cd -
 rm -rf rpm
 
 mkdir deb
 cd deb
-aws s3 cp --recursive --exclude="*" --include="*.deb" "${Env:DEB_PATH}" . --no-sign-request
-Get-ChildItem -File -Recurse | Set-AzStorageBlobContent -Container 'senzing' -Context $StorageAccount.Context
+packages=$(aws s3 ls ${Env:DEB_PATH} --no-sign-request | awk '{print $NF}' | grep "${Env:SENZING_VERSION}" | grep '.deb')
+for package in ${packages}; do
+  echo "[INFO] download: $package"
+  aws s3 cp --exclude="*" --include="$package" "${Env:DEB_PATH}" . --no-sign-request
+  Set-AzStorageFileContent -ShareName 'senzing' -Source "$package" -Path "${Env:ARCHITECTURE}/openssl${Env:OPENSSLVERSION}" -Context $storageAccount.Context
+done
 cd -
+rm -rf deb
 
 #echo "[INFO] Invoke-RestMethod -Uri ${Env:RPM_PATH}/senzingapi-${Env:SENZING_VERSION}.${Env:RPM_ARCHITECTURE}.rpm -OutFile senzingapi-${Env:SENZING_VERSION}.${Env:RPM_ARCHITECTURE}.rpm"
 #Invoke-RestMethod -Uri "${Env:RPM_PATH}/senzingapi-${Env:SENZING_VERSION}.${Env:RPM_ARCHITECTURE}.rpm" -OutFile "senzingapi-${Env:SENZING_VERSION}.${Env:RPM_ARCHITECTURE}.rpm"
