5cac56ddfe2b0962a150202fd552ada2544897ad
Sam <109683132+kernelsam@users.noreply.github.com>
2025-03-09 17:18:25 -0700

    debug

--- azure-test/azuretest.ps1
+++ azure-test/azuretest.ps1
@@ -140,10 +140,13 @@ cd $localFolder
 #  echo "[INFO] aws s3 sync --exclude=* --include=$package ${Env:RPM_PATH} . --no-sign-request"
 #  aws s3 sync --exclude="*" --include="$package" "${Env:RPM_PATH}" . --no-sign-request
   $CurrentFolder = (Get-Item .).FullName
+  ls -tlc
+  pwd
+  ls -tlc x86/openssl3/
   $Container = Get-AzStorageShare -Name 'senzing' -Context $StorageAccount.Context
   Get-ChildItem -Recurse | Where-Object { $_.GetType().Name -eq "FileInfo"} | ForEach-Object {
     $path=$_.FullName.Substring($Currentfolder.Length+1).Replace("\","/")
-    Write-Host "[INFO] Set-AzStorageFileContent -ShareClient $Container -Source $_.FullName -Path $path -Force -Context $StorageAccount.Context"
+    Write-Host "[INFO] Set-AzStorageFileContent -ShareClient $Container -Source $_.FullName -Path $path -Force -Context"
     Set-AzStorageFileContent -ShareClient $Container -Source $_.FullName -Path $path -Force -Context $StorageAccount.Context
   }
   #Set-AzStorageFileContent -ShareName 'senzing' -Source "$package" -Path "${Env:ARCHITECTURE}/openssl${Env:OPENSSLVERSION}" -Context $storageAccount.Context
