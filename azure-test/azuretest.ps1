5505fa131df6977f2a62d2c404dce3857cbe056a
Sam <109683132+kernelsam@users.noreply.github.com>
2025-03-17 14:07:39 -0700

    test curl

--- azure-test/azuretest.ps1
+++ azure-test/azuretest.ps1
@@ -96,7 +96,7 @@ $containerUrl = "https://senzing.blob.core.windows.net/senzing/${Env:ARCHITECTUR
 # Get an access token for managed identities for Azure resources
 $response = Invoke-WebRequest -Uri 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fmanagement.azure.com%2F' `
                               -Headers @{Metadata="true"}
-echo $response
+curl 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fmanagement.azure.com%2F' -H Metadata:true -s  
 $content =$response.Content | ConvertFrom-Json
 $access_token = $content.access_token
 echo "The managed identities for Azure resources access token is $access_token"
