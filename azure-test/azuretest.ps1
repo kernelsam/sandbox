c38061f6aeca1c866cf140b35b674f85bcd26c35
Sam <109683132+kernelsam@users.noreply.github.com>
2025-03-17 11:56:18 -0700

    remove print

--- azure-test/azuretest.ps1
+++ azure-test/azuretest.ps1
@@ -94,7 +94,7 @@ df -h
 $containerUrl = "https://senzing.blob.core.windows.net/senzing/${Env:ARCHITECTURE}/openssl${Env:OPENSSLVERSION}" 
 
 $response=$(curl 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fmanagement.azure.com%2F' -H Metadata:true -s)
-print $response
+echo $response
 $access_token=$(echo $response | python -c 'import sys, json; print (json.load(sys.stdin)["access_token"])')
 echo The managed identities for Azure resources access token is $access_token
 
