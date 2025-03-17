def32cc2f7ea49f23eea446dae1499fa7eca3541
Sam <109683132+kernelsam@users.noreply.github.com>
2025-03-17 10:42:32 -0700

    test

--- azure-test/azuretest.ps1
+++ azure-test/azuretest.ps1
@@ -9,8 +9,10 @@ tar -xvzf azcopy_v10.tar.gz --strip-components=1
 ./azcopy --version
 $AZCOPY_AUTO_LOGIN_TYPE="MSI"
 $AZCOPY_MSI_CLIENT_ID=${Env:CLIENTID}
-$AZCOPY_MSI_OBJECT_ID=${Env:OBJECTID}
-$AZCOPY_MSI_RESOURCE_STRING=${Env:RESOURCEID}
+#$AZCOPY_MSI_OBJECT_ID=${Env:OBJECTID}
+#$AZCOPY_MSI_RESOURCE_STRING=${Env:RESOURCEID}
+echo "principal id: ${Env:PRINCIPALID}"
+echo "tenant id: ${Env:TENANTID}"
 $AZCOPY_REQUEST_TRY_TIMEOUT=15
 #export AZCOPY_MSI_OBJECT_ID=<object-id>
 #export AZCOPY_MSI_RESOURCE_STRING=<resource-id>
