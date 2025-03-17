ed69a5121fad8ae177ba05888e66770a55ff73a7
Sam <109683132+kernelsam@users.noreply.github.com>
2025-03-17 12:02:04 -0700

    use powershell

--- azure-test/azuretest.ps1
+++ azure-test/azuretest.ps1
@@ -93,10 +93,13 @@ df -h
 
 $containerUrl = "https://senzing.blob.core.windows.net/senzing/${Env:ARCHITECTURE}/openssl${Env:OPENSSLVERSION}" 
 
-$response=$(curl 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fmanagement.azure.com%2F' -H Metadata:true -s)
+# Get an access token for managed identities for Azure resources
+$response = Invoke-WebRequest -Uri 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fmanagement.azure.com%2F' `
+                              -Headers @{Metadata="true"}
 echo $response
-$access_token=$(echo $response | python -c 'import sys, json; print (json.load(sys.stdin)["access_token"])')
-echo The managed identities for Azure resources access token is $access_token
+$content =$response.Content | ConvertFrom-Json
+$access_token = $content.access_token
+echo "The managed identities for Azure resources access token is $access_token"
 
 echo "[INFO] ./azcopy cp $containerUrl /tmp --recursive --log-level=DEBUG"
 ./azcopy cp "$containerUrl" /tmp --recursive --log-level=DEBUG
